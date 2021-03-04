07eda7b4b0d6ce540d397ed3ac95f7f3ade7e119
silence <silence_zhpf@aliyun.com>
2021-03-04 18:05:41 +0800

    feat(mod): add the tips no matter success or fail

--- mod.ts
+++ mod.ts
@@ -1,9 +1,15 @@
 await genService();
 
 async function genService() {
-    await Deno.mkdir("service")
-    Deno.writeTextFile("service/index.ts", "");
-    Deno.writeTextFile("service/fetch.ts", "");
-    Deno.writeTextFile("service/fun.ts", "");
-    Deno.writeTextFile("service/fun.test.ts", "");
+  await Deno.mkdir("service");
+  Promise.all([
+    Deno.writeTextFile("service/index.ts", ""),
+    Deno.writeTextFile("service/fetch.ts", ""),
+    Deno.writeTextFile("service/fun.ts", ""),
+    Deno.writeTextFile("service/fun.test.ts", ""),
+  ]).then(() => {
+    console.log("Create Service,Success");
+  }).catch((e) => {
+    console.error(e);
+  });
 }
