b9db8875f847d425f144c8c8b3266be17aaf845d
silence <silence_zhpf@aliyun.com>
2021-03-04 10:23:31 +0800

    feat(mod): 添加生成 service脚本

--- mod.ts
+++ mod.ts
@@ -1,1 +1,9 @@
+await genService();
 
+async function genService() {
+    await Deno.mkdir("service")
+    Deno.writeTextFile("service/index.ts", "");
+    Deno.writeTextFile("service/fetch.ts", "");
+    Deno.writeTextFile("service/fun.ts", "");
+    Deno.writeTextFile("service/fun.test.ts", "");
+}
