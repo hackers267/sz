00ead19239fd263f4bf883e1e4975007adee2be6
silence <silence_zhpf@aliyun.com>
2021-03-05 14:19:24 +0800

    fix(mod): fix the bug which the existed file was overload

--- mod.ts
+++ mod.ts
@@ -1,16 +1,34 @@
 import { green, red } from "./deps.ts";
-await genService();
 
-async function genService() {
-  await Deno.mkdir("service");
-  Promise.all([
-    Deno.writeTextFile("service/index.ts", ""),
-    Deno.writeTextFile("service/fetch.ts", ""),
-    Deno.writeTextFile("service/fun.ts", ""),
-    Deno.writeTextFile("service/fun.test.ts", ""),
-  ]).then(() => {
-    console.log(green("Create Service,Success"));
-  }).catch((e) => {
-    console.log(red(e));
-  });
+const files = ["index.ts", "fetch.ts", "fun.ts", "fun.test.ts"];
+
+await genService(files);
+
+async function genService(files: string[]) {
+  try {
+    await Deno.mkdir("service");
+  } catch (e) {
+    console.log(red("mkdir dir error,because the dir existed!"));
+  } finally {
+    const filePromises = files.map((x) => `service/${x}`).filter((x) =>
+      fileExist(x)
+    ).map((x) => {
+      Deno.writeTextFile(x, "");
+      return x;
+    });
+    Promise.all(filePromises).then((strings) => {
+      strings.forEach((s) => console.log(green(`Create ${s} Service,Success`)));
+    }).catch(() => {
+      console.log(red("Something Error"));
+    });
+  }
+}
+
+function fileExist(file: string) {
+  try {
+    Deno.statSync(file);
+    return false;
+  } catch (e) {
+    return true;
+  }
 }
